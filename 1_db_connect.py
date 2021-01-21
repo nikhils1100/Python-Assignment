a5671061e8dd74f2182c0b466f82a635c29f5ba4
Nik <sharmanikhil1100@gmail.com>
2021-01-21 17:51:34 +0530

    Made db_connect into Class object

--- 1_db_connect.py
+++ 1_db_connect.py
@@ -2,23 +2,31 @@ import MySQLdb
 import MySQLdb.cursors as cursors
 import traceback
 
-def getRows():
-    conn = MySQLdb.connect(host="localhost",user="root",
-                  passwd="",db="employees", cursorclass=cursors.SSCursor)
-    # cursorclass = cursors.SSCursor. No need to create memory block, so using this specific parameter as it stores memory on server
+class db_connect:
+    def __init__(self, uname, password, db_name):
+        self.uname = uname 
+        self.password = password
+        self.db_name = db_name
 
-    dbCursor = conn.cursor()
+    def getRows(self, query):
+        conn = MySQLdb.connect(host="localhost",user=self.uname,
+                    passwd=self.password,db=self.db_name, cursorclass=cursors.SSCursor)
+        # cursorclass = cursors.SSCursor. No need to create memory block, so using this specific parameter as it stores memory on server
 
-    query = 'SELECT first_name FROM employees'
-    try:
-        dbCursor.execute(query)
-        for row in dbCursor:
-            yield row
-    except Exception as e:
-        traceback.print_exc()
+        dbCursor = conn.cursor()
 
-i = 0
-for row in getRows():
-    print(row)
-    i += 1
-print(i)
+        try:
+            dbCursor.execute(query)
+            for row in dbCursor:
+                yield row
+        except Exception as e:
+            traceback.print_exc()
+
+if __name__ == '__main__':
+    db_instance = db_connect('root', '', 'employees')
+    query = 'SELECT first_name, salary, from_date FROM employees INNER JOIN salaries on employees.emp_no = salaries.emp_no'
+    i = 0
+    for row in db_instance.getRows(query):
+        print(row)
+        i += 1
+    print('Number of rows: '+str(i))
